82e72a9483961a668c47a019955825c4ff0e990e
DESKTOP-BSD5S3P\ivanm <ivamolmal@gmail.com>
2019-05-25 21:36:56 +0200

    PageObjectdeliveryIInformation.js + Reporter

--- tests/Screens/PageObjecHome.js
+++ tests/Screens/PageObjecHome.js
@@ -4,5 +4,6 @@ export default class Principal {
     constructor () {
         this.url = "https://demo.oscommerce.com/";
         this.samsungGalaxyTab = Selector('a').withText('Samsung Galaxy Tab');
+        this.checkOutButton = Selector('#tdb2');
     }
 }
--- tests/TestSuites/Buyers.js
+++ tests/TestSuites/Buyers.js
@@ -1,22 +1,32 @@
 import Principal from '../Screens/PageObjecHome';
 import ProductSheet from '../Screens/PageObjectProductSheet';
 import CartContents from '../Screens/PageObjectCartContents';
+import SignIn from '../Screens/PageObjectSignIn';
 
 const principal = new Principal();
 const productSheet = new ProductSheet("Samsung Galaxy Tab");
 const cartContents = new CartContents();
+const signIn = new SignIn();
 
-fixture('Prueba Tests')
+fixture('OSCommerce Demo')
     .page(principal.url);
 
 test('Buy Product', async t => {
     await t
+    //Se puede escoger el tamaño específico de lapantalla según el dispositivo
+    /*.resizeWindowToFitDevice('Sony Xperia Z', {
+        portraitOrientation: true
+    })*/
+    .maximizeWindow()
     .click(principal.samsungGalaxyTab)
     .click(productSheet.addToCartButton)
     .click(cartContents.cartQuantity)
     .pressKey('ctrl+a delete')
     .typeText(cartContents.cartQuantity, '5')
     .click(cartContents.updateCartQuantity)
-    .click(cartContents.checkoutButton);
+    .click(cartContents.checkoutButton)
+    .typeText(signIn.emailAddress, 'ivanmolmar@outlook.es')
+    .typeText(signIn.password, 'paswordfalso123')
+    .click(signIn.signInButton);
     //.expect(cartContents.cartQuantity).eql('5');
 });
